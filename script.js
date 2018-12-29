04bae4dadc7171a0146eccd9fd9dfabfc097b866
Engivirus <vittorio.minuzzo@gmail.com>
2018-12-29 17:42:56 +0100

    .

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+function fetch_food()
+{
+	var url = "http://engivirus.github.io/wiki/food.json"
+
+	var request = new XMLHttpRequest();
+	request.open('GET', url);
+	request.send();
+
+	function onLoad(event)
+	{
+		if(request.status >= 400)
+			onFail(event);
+		else
+		{
+			var json = JSON.parse(this.responseText);
+			callback(null, json);
+		}
+	}
+
+	function onFail(event)
+		callback(new Error("Error occurred."));
+}
+
+
+
+food_dict = fetch_food();
