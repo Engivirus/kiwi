70c2c1fa4d5bc5bce7b1ef32ab958a6315721c5b
Engivirus <vittorio.minuzzo@gmail.com>
2018-12-29 18:19:47 +0100

    .

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 
 
-function fetch_food()
+var food_dict = null;
+
+function fetch_json(url)
 {
-	var url = "https://engivirus.github.io/kiwi/food.json"
-
-	var request = new XMLHttpRequest();
-	request.open('GET', url);
-	request.send();
-
-	function onLoad(event)
-	{
-		if(request.status >= 400)
-			onFail(event);
-		else
-		{
-			var json = JSON.parse(this.responseText);
-			callback(null, json);
-			return json;
-		}
-	}
-
-	function onFail(event)
-	{
-		callback(new Error("Error occurred."));
-	}
+	fetch(url)
+	.then(response => response.json())
+	.then(data => {
+		console.log(data) // Prints result from `response.json()` in getRequest
+	})
+	.catch(error => console.error(error))
 }
 
 
 
-food_dict = fetch_food();
-alert(food_dict);
+dummy_obj = JSON.parse('{"one": 1, "two": 2, "three": 3}');
+
+
+var url = "https://engivirus.github.io/kiwi/food.json"
+fetch_json(url);
+// food_dict = fetch_json(url);
+
+
+// alert(food_dict);
+alert(dummy_obj);
