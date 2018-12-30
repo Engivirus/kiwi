e94547e16999ae13fa15ef03484b012a67979505
Engivirus <vittorio.minuzzo@gmail.com>
2018-12-30 11:10:30 +0100

    Fixed empty lines and unrecognized food

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function get_text_entries(item)
 	var entries = [];
 
 	var lines = item.value.split('\n');
-	console.log(lines);
+	// console.log(lines);
 	for(var i=0; i<lines.length; i++)
 	{
 		if(lines[i].includes(' '))
@@ -39,12 +39,12 @@ function get_text_entries(item)
 		}
 		else
 		{
-			var new_pair = ["empty", 0];
+			var new_pair = ["", 0];
 			entries.push(new_pair);
 		}
 	}
 
-	console.log(lines);
+	// console.log(lines);
 	return entries;
 }
 
@@ -56,23 +56,23 @@ function create_label_array(entries)
 	for(var i=0; i<entries.length; i++)
 	{
 		var foodname = entries[i][0];
-		console.log(foodname);
-		if(foodname === "")
+		// console.log(foodname);
+		if(foodname == "")
 		{
-			calories_array.push(-1);
+			calories_array.push(-1); // empty line
 		}
 		else if(foodlist.hasOwnProperty(foodname))
 		{
 			var amount = foodlist[foodname] / 100 * entries[i][1];
-			calories_array.push(amount);
+			calories_array.push(Math.round(amount));
 		}
 		else
 		{
-			calories_array.push(0);
+			calories_array.push(-2); // food not in database
 		}
 	}
 
-	console.log(calories_array);
+	// console.log(calories_array);
 	return calories_array;
 }
 
@@ -93,10 +93,14 @@ function set_labels(array)
 		// console.log(array[i]);
 		
 		var new_div = document.createElement("div");
-		var new_content = document.createTextNode(array[i]);
+
+		var text = array[i].toString();
+		if(array[i] === -1)
+			text = "_";
+		else if(array[i] === -2)
+			text = "?";
 		
-		if(array[i] < 0)
-			new_content = document.createTextNode("");
+		new_content = document.createTextNode(text);
 
 		new_div.appendChild(new_content);
 		
@@ -114,10 +118,26 @@ function update_labels()
 	set_labels(array);
 }
 
+function update_total()
+{
+	var labels = document.querySelector("#labels").children;
+	var total = 0;
+
+	for(var i=0; i<labels.length; i++)
+	{
+		// console.log(labels[i]);
+		if(labels[i].innerText != "_" && labels[i].innerText != "?")
+			total += parseFloat(labels[i].innerText);
+	}
+
+	var totalcal_div = document.querySelector("#totalcal h2");
+	totalcal_div.innerText = total+" kcal";
+}
+
 function parse_amount(somestring)
 {
 	if(somestring.includes("kg"))
-	   return 1000 * parseInt(somestring.replace("kg",""));
+		return 1000 * parseFloat(somestring.replace("kg",""));
 	else
 		return parseInt(somestring.replace("g",""));
 }
@@ -133,7 +153,8 @@ fetch_json("https://engivirus.github.io/kiwi/food.json");
 set_labels([]);
 
 
-textarea.addEventListener("keyup", function() { update_labels() });
+textarea.addEventListener("keyup", function() { update_labels(); update_total(); });
+// var intervalID = window.setInterval(function() { update_labels(); update_total(); }, 1000);
 
 
 
